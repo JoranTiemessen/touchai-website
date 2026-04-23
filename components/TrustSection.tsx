0ea0d6a960aff465e16b98b6de14e5cc9d3fc06f
JoranTiemessen <info@touchaiagency.com>
2026-04-23 10:55:05 +0200

    Show client logos in brand colors (remove grayscale)

--- components/TrustSection.tsx
+++ components/TrustSection.tsx
@@ -32,7 +32,7 @@ export default function TrustSection() {
                 <img
                   src={c.logo}
                   alt={c.name}
-                  className="h-10 w-auto max-w-[140px] object-contain opacity-60 grayscale hover:opacity-100 hover:grayscale-0 transition duration-300"
+                  className="h-10 w-auto max-w-[140px] object-contain opacity-90 hover:opacity-100 transition duration-300"
                 />
                 <span
                   className="text-sm font-semibold tracking-wide text-gray-500"
